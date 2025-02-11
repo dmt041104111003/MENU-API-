805d366719f9e954dac4cc982ca26d757c647fd4
Đào Mạnh Tùng <141070994+dmt041104111003@users.noreply.github.com>
2025-02-11 17:53:40 +0700

    Add files via upload

---
+++ daomanhtung/resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by daomanhtung.rc
+//
+#define IDC_MYICON                      2
+#define IDD_DAOMANHTUNG_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDC_POINTER                     104
+#define IDC_MAIN                        104
+#define IDM_EXIT                        105
+#define IDI_DAOMANHTUNG                 107
+#define IDI_SMALL                       108
+#define IDC_DAOMANHTUNG                 109
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDR_MENU2                       131
+#define IDM_OPEN                        222
+#define IDM_NEW                         333
+#define ID_LANGUAGES_NEW                32771
+#define ID_LANGUAGES_OPEN               32772
+#define ID_LANGUAGES_SAVE               32773
+#define ID_LANGUAGES_CLOSE              32774
+#define ID_FILE_NEW                     32775
+#define ID_FILE_OPEN                    32776
+#define ID_FILE_EXIT                    32777
+#define ID_LANGUAGES_ENGLISH            32778
+#define ID_LANGUAGES_VIETNAMESE         32779
+#define ID_T32785                       32785
+#define ID_T32786                       32786
+#define ID_T32787                       32787
+#define ID_NG32788                      32788
+#define ID_NG32789                      32789
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32794
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
